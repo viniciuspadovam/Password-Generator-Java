53e9e4eb4a1aea8df46bd0567fed4023b72140e9
viniciuspadovam <padovam3373@gmail.com>
2022-09-26 17:49:50 -0300

    clear console fix

--- PassGenerator.java
+++ PassGenerator.java
@@ -52,9 +52,7 @@ public class PassGenerator {
 
     public static void clearConsole() {
         try {
-            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-          } catch(Exception e) {
-            e.printStackTrace();
-        }
+            new ProcessBuilder("cmd", "/c", "cls", "clear").inheritIO().start().waitFor();
+        } catch(Exception e) {}
     }
 }
